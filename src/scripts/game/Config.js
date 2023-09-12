d8bac535276c04c94288f9ee0e225be2865f4aed
Danny-Devs <et3rnal.d@gmail.com>
2023-09-12 07:27:47 -0700

    first commit

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -1,9 +1,18 @@
-import { Game } from "./Game";
-import { Tools } from "../system/Tools";
+import { Game } from './Game';
+import { Tools } from '../system/Tools';
 
 export const Config = {
-    loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
-    scenes: {
-        "Game": Game
+  loader: Tools.massiveRequire(
+    require['context']('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)
+  ),
+  scenes: {
+    Game: Game
+  },
+  bgSpeed: 2,
+  hero: {
+    position: {
+      x: 350,
+      y: 545
     }
+  }
 };
--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,14 +1,32 @@
-import { App } from "../system/App";
-import { Scene } from "../system/Scene";
+import { App } from '../system/App';
+import { Background } from './Background';
+import { Platform } from './Platform';
+import { Scene } from '../system/Scene';
+import { Hero } from './Hero';
 
 export class Game extends Scene {
-    create() {
-        this.createBackground();
-    }
-    createBackground() {
-        this.bg = App.sprite("bg");
-        this.bg.width = window.innerWidth;
-        this.bg.height = window.innerHeight;
-        this.container.addChild(this.bg);
-    }
+  create() {
+    this.createBackground();
+    this.createPlatform({
+        rows: 4,
+        cols: 6,
+        x: 200
+    });
+    this.createHero();
+  }
+  createPlatform(data) {
+    const platform = new Platform(data.rows, data.cols, data.x);
+    this.container.addChild(platform.container);
+  }
+  createBackground() {
+    this.bg = new Background();
+    this.container.addChild(this.bg.container);
+  }
+  createHero() {
+    this.hero = new Hero();
+    this.container.addChild(this.hero.sprite);
+  }
+  update(dt) {
+    this.bg.update(dt);
+  }
 }
